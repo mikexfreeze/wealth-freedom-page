9fffb1d580b0ab87d3d8ee56ab38a9b7c13afcfc
mikexfreeze <408026512@qq.com>
2021-02-25 08:37:53 +0000

    Deploying to gh-pages from @ mikexfreeze/freedom-from-wealth@26eaaf811541e25ea9b9b6d719e83451d5803cae 🚀

---
+++ pages/index/index.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="content">
+		<view>
+			<navigator url="/pages/calculator/calculator">uniCloud操作演示</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/password/password">用户名和密码</navigator>
+		</view>
+		<!-- <view>
+			<navigator url="/pages/mobile/mobile">手机号和验证码</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/email/email">邮箱与验证码</navigator>
+		</view> -->
+		<!-- #ifdef APP-PLUS || MP-WEIXIN -->
+		<view>
+			<navigator url="/pages/weixin/weixin">微信登录</navigator>
+		</view>
+		<!-- #endif -->
+		<!-- #ifdef MP-ALIPAY -->
+		<view>
+			<navigator url="/pages/alipay/alipay">支付宝登录</navigator>
+		</view>
+		<!-- #endif -->
+		<!-- <view>
+			<navigator url="/pages/util/util">通用方法</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/rbac/rbac">角色权限</navigator>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		padding: 15px;
+	}
+</style>
